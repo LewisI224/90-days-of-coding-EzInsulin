e7a8e385fca78abf02ee681ebf37adede1421836
Lewis Inches <incheslewis@gmail.com>
2022-06-29 23:46:35 +0100

    Added more input fields and UI for tooltips and conversion

--- app/src/main/java/uk/co/lewisinches/ezinsulin/MainActivity.kt
+++ app/src/main/java/uk/co/lewisinches/ezinsulin/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlin.math.ceil
+import kotlin.math.floor
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,22 +27,38 @@ class MainActivity : AppCompatActivity() {
         val calculateButton = findViewById<Button>(R.id.Calculate_Button)
 
         calculateButton.setOnClickListener {
-            val carbs = findViewById<EditText>(R.id.CHO_Number_Input)
-            val insulinRatio = findViewById<EditText>(R.id.IR_Number_Input)
-            val carbsNumber = carbs.text.toString().toInt()
-            val insulinRatioNumber = insulinRatio.text.toString().toDouble()
-            val dose = calculateDose(carbsNumber, insulinRatioNumber)
+
+            //retrieve values from inputs
+            val InsulinRatio = findViewById<EditText>(R.id.IR_Number_Input)
+            val Carbs = findViewById<EditText>(R.id.CHO_Number_Input)
+            val CurrBS = findViewById<EditText>(R.id.currBS)
+            val TarBS = findViewById<EditText>(R.id.tarBS)
+            val CF = findViewById<EditText>(R.id.CF)
+
+            //convert inputs from string to doubles
+            val carbsNumber = Carbs.text.toString().toDouble()
+            val insulinRatioNumber = InsulinRatio.text.toString().toDouble()
+            val CurrBSNumber = CurrBS.text.toString().toDouble()
+            val TarBSNumber = TarBS.text.toString().toDouble()
+            val CFNumber = CF.text.toString().toDouble()
+
+            //pass values to dose calculator
+            val dose = calculateDose(carbsNumber, insulinRatioNumber, CurrBSNumber, TarBSNumber, CFNumber)
+            //set ui output to returned dose values
             val doseText = findViewById<TextView>(R.id.Units_label)
             doseText.text = ("$dose units")
         }
     }
 
 
-    private fun calculateDose(carbs: Int, insulinRatio: Double): String {
-//        dose = (Insulin Ratio / Carbs) + ((Current BS - Target BS)/ Correction Factor)
-        return String.format("%.1f", carbs/insulinRatio)
+    private fun calculateDose(CHO: Double, IR: Double, CurrBS: Double, TarBS: Double, CF: Double): String {
+        var carbDose = CHO/IR
+        carbDose = floor(carbDose * 2) /2 .toDouble()
+        var corrDose = (CurrBS - TarBS)/ CF
+        corrDose = floor(corrDose * 2) /2 .toDouble()
+        val totalDose = carbDose + corrDose
 
-
+        return totalDose.toString()
     }
 
 }
